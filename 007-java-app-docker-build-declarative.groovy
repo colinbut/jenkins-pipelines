3a55812b0e0728b0f78caaea69c83ad5d964d9be
Colin But <colin.but@outlook.com>
2020-04-27 16:38:47 +0100

    a simple docker build pipeline declarative style for a java app

---
+++ 007-java-app-docker-build-declarative.groovy
@@ -0,0 +1,45 @@
+#!/usr/bin/env groovy
+pipeline {
+    agent any
+    stages {
+        stage("Compile") {
+            steps {
+                sh "./mvnw clean compile"
+            }
+        }
+        stage("Unit Tests") {
+            steps {
+                sh "./mvnw test"
+            }
+        }
+        stage("Integration Tests") {
+            steps {
+                sh "./mvnw verify"
+            }
+        }
+        stage("Package Artifact") {
+            steps {
+                sh "./mvnw package -DskipTests=true"
+            }
+        }
+        stage ('Build Docker Image') {
+            environment {
+                COMMIT_ID = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+            }
+            steps {
+                echo "Building a Docker image..."
+                sh ('docker build -t example-app:${COMMIT_ID} .')
+            }
+        }
+        stage ('Publish Docker image') {
+            steps {
+                script {
+                    // uses Docker Jenkins Plugin
+                    docker.withRegistry('https://066203203749.dkr.ecr.eu-west-2.amazonaws.com', 'ecr:eu-west-2:AWS_CREDENTIALS') {
+                        docker.image('example-app:${COMMIT_ID}').push()
+                    }
+                }
+            }
+        }
+    }
+}
