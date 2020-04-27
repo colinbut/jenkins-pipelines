e7e3e1cde94fae6496d1728173c685629a22cc62
Colin But <colin.but@outlook.com>
2020-04-27 16:38:33 +0100

    a full build pipeline declarative style for a java app from start to finish publish artifacts to artfiactory

---
+++ 006-java-app-noncontainerized-full-pipeline-artifactory-declarative.groovy
@@ -0,0 +1,65 @@
+#!/usr/bin/env groovy
+pipeline {
+    agent any
+    stages {
+        stage("Compile") {
+            steps {
+                sh "./mvnw clean compile"
+            }
+        }
+        stage("Unit Tests") {
+            steps {
+                sh "./mvnw test"
+            }
+        }
+        stage("Integration Tests") {
+            steps {
+                sh "./mvnw verify"
+            }
+        }
+        stage("Package") {
+            steps {
+                sh "./mvnw package -DskipTests=true"
+            }
+        }
+        stage ('Static Code Analysis: SonarQube') {
+            steps {
+                // using SonarQube Jenkins Plugin
+                // SonarQube Credentials configured under Jenkins Credentials
+                // SonarQube server configured in Jenkins Global System Configuration
+                withSonarQubeEnv(credentialsId: 'sonarqube-credentials', installationName: 'MySonarQubeServer') {
+                    sh './mvnw sonar:sonar'
+                }
+            }
+        }
+        stage('Publish Artifact to Artifactory') {
+            steps {
+                rtBuildInfo(captureEnv: true)
+
+                rtMavenResolver (
+                        id: 'resolver-unique-id',
+                        serverId: 'ART',
+                        releaseRepo: 'libs-release',
+                        snapshotRepo: 'libs-snapshot'
+                )
+                rtMavenDeployer(
+                        id: 'deployer-unique-id',
+                        serverId: 'ART',
+                        releaseRepo: 'libs-release',
+                        snapshotRepo: 'libs-snapshot'
+                )
+
+                rtMavenRun(
+                        tool: "Maven 3.6.0", // using Maven configured under Jenkins Global Tools
+                        pom: "pom.xml",
+                        goals: "clean install -DskipTests=true",
+                        resolverId: 'resolver-unique-id',
+                        deployerId: 'deployer-unique-id'
+                )
+
+                rtPublishBuildInfo(serverId: 'ART')
+
+            }
+        }
+    }
+}
