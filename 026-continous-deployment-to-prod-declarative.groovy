0819de19b950fdb80d3872ac8701840b5e8680fe
Colin But <colin.but@outlook.com>
2020-04-29 08:02:03 +0100

    a full CD (Continuous Deployment) pipeline which builds & deploys to prod

---
+++ 026-continous-deployment-to-prod-declarative.groovy
@@ -0,0 +1,77 @@
+#!/usr/bin/env groovy
+pipeline {
+    agent any
+    stages {
+        stage("Compile") {
+            steps {
+                sh "./mvnw clean compile"
+            }
+        }
+        stage("Unit Tests") {
+            steps {
+                sh "./mvnw test"
+            }
+        }
+        stage("Integration Tests") {
+            steps {
+                sh "./mvnw verify"
+            }
+        }
+        stage("Package") {
+            steps {
+                sh "./mvnw package -DskipTests=true"
+            }
+        }
+        stage ('Static Code Analysis: SonarQube') {
+            when {
+                branch 'master'
+            }
+            steps {
+                // using SonarQube Jenkins Plugin
+                // SonarQube Credentials configured under Jenkins Credentials
+                // SonarQube server configured in Jenkins Global System Configuration
+                withSonarQubeEnv(credentialsId: 'sonarqube-credentials', installationName: 'MySonarQubeServer') {
+                    sh './mvnw sonar:sonar'
+                }
+            }
+        }
+        stage ('Build Docker Image') {
+            when {
+                branch 'master'
+            }
+            environment {
+                COMMIT_ID = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+            }
+            steps {
+                echo "Building a Docker image..."
+                sh ('docker build -t 066203203749.dkr.ecr.eu-west-2.amazonaws.com/example-app:${COMMIT_ID} .')
+            }
+        }
+        stage ('Publish Docker image') {
+            when {
+                branch 'master'
+            }
+            steps {
+                script {
+                    // uses Docker Jenkins Plugin
+                    docker.withRegistry('https://066203203749.dkr.ecr.eu-west-2.amazonaws.com', 'ecr:eu-west-2:AWS_CREDENTIALS') {
+                        docker.image('066203203749.dkr.ecr.eu-west-2.amazonaws.com/example-app:${COMMIT_ID}').push()
+                    }
+                }
+            }
+        }
+        stage ("Deploy to Production") {
+            when {
+                beforeInput true
+                branch 'master'
+            }
+            input {
+                message "Deploy to Prod?"
+                ok "Yes, Approved"
+            }
+            steps {
+                echo "Deploying to Production"
+            }
+        }
+    }
+}
