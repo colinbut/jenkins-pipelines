8c5a29e812e420312d12cb64cbd1dc074a2dbfe4
Colin But <colin.but@outlook.com>
2020-04-29 08:01:28 +0100

    a very simple scripted pipeline

---
+++ 027-simple-build-scripted.groovy
@@ -0,0 +1,22 @@
+#!/usr/bin/env groovy
+node {
+    stage("Source") {
+        git credentialsId: "github_credentials", url: "https://github.com/colinbut/jenkins-pipelines.git"
+    }
+
+    def maven = tool 'apache-maven-3.6.3'
+
+    stage("Compile") {
+        sh "${maven}/bin/mvn clean compile"
+    }
+    stage("Unit Tests") {
+
+        sh "${maven}/bin/mvn test"
+    }
+    stage("Integration Tests") {
+        sh "${maven}/bin/mvn verify"
+    }
+    stage("Package") {
+        sh "${maven}/bin/mvn package -DskipTests=true"
+    }
+}
