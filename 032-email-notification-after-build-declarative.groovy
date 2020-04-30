f5680497fad82f09ac378bf34a10a0aabe39cc3d
Colin But <colin.but@outlook.com>
2020-04-30 18:49:00 +0100

    sending email notification after build has finished - declarative example

---
+++ 032-email-notification-after-build-declarative.groovy
@@ -0,0 +1,55 @@
+#!/usr/bin/env groovy
+pipeline {
+    agent any
+    stages {
+        stage("Compile") {
+            steps {
+                sh "./mvnw clean compile"
+            }
+        }
+        stage("Unit Tests") {
+            steps {
+                sh "./mvnw test"
+            }
+        }
+        stage("Integration Tests") {
+            steps {
+                sh "./mvnw verify"
+            }
+        }
+        stage("Package") {
+            steps {
+                sh "./mvnw package -DskipTests=true"
+            }
+        }
+    }
+    post {
+        always {
+            echo "The build ${env.JOB_NAME} #${env.BUILD_NUMBER} has finished"
+            emailext(
+                    body: "The build ${env.JOB_NAME} #${env.BUILD_NUMBER} has finished",
+                    recipientProviders:[
+                            [
+                                    $class: 'DevelopersRecipientProvider'
+                            ],
+                            [
+                                    $class: 'RequesterRecipientProvider'
+                            ]
+                    ],
+                    subject: "${env.JOB_NAME}"
+            )
+        }
+        aborted {
+            echo "The build ${env.JOB_NAME} #${env.BUILD_NUMBER} has been aborted"
+        }
+        failure {
+            echo "The build ${env.JOB_NAME} #${env.BUILD_NUMBER} has failured"
+        }
+        success {
+            echo "The build ${env.JOB_NAME} #${env.BUILD_NUMBER} has successfully been built"
+        }
+        cleanup {
+            echo "Cleaning up build: ${env.BUILD_URL}"
+        }
+    }
+}
