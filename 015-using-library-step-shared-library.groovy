5c8f7a23f9b9b21ec5b47a0d53fd7d08c55116e8
Colin But <colin.but@outlook.com>
2020-04-30 19:11:18 +0100

    using a library step to override shared library version

--- 015-using-library-step-shared-library.groovy
+++ 015-using-library-step-shared-library.groovy
@@ -1,3 +1,3 @@
-library 'my-shared-library-allow-override@master'
+library 'my-shared-library-allow-override@0.0.1'
 
 buildJavaAppDockerFull(repo: "jenkins-pipelines", microserviceName: "example-app")
