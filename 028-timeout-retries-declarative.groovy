9f413f89d6ebb554d5c4b742c8505d6e1e9b8bb4
Colin But <colin.but@outlook.com>
2020-04-30 19:22:23 +0100

    timeout and retries example with a declarative pipeline

---
+++ 028-timeout-retries-declarative.groovy
@@ -0,0 +1,34 @@
+#!/usr/bin/env groovy
+pipeline {
+    agent any
+    stages {
+        stage("Compile") {
+            steps {
+                sh "mvn clean compile"
+            }
+        }
+        stage("Unit Tests") {
+            options {
+                timeout(time: 2, unit: "MINUTES")
+            }
+            steps {
+                sh "mvn test"
+            }
+        }
+        stage("Integration Tests") {
+            steps {
+                retry(3) {
+                    sh "mvn verify"
+                }
+                timeout(time: 10, unit: "SECONDS") {
+                    sh "mvn verify"
+                }
+            }
+        }
+        stage("Package") {
+            timeout(time: 1, unit: "MINUTES") {
+                sh "mvn package -DskipTests=true"
+            }
+        }
+    }
+}
